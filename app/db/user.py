d54e155bfe18b4c1c7c5d6ac337d8c4469cf2bdb
Alastair Paragas <alastairparagas@gmail.com>
2017-06-11 16:43:35 -0400

    User Model: unit-tested

--- app/db/user.py
+++ app/db/user.py
@@ -1,10 +1,21 @@
 import aiopg
 import pytz
-from typing import NamedTuple, Union, List
+from typing import NamedTuple, Union, List, Tuple
 from datetime import datetime
 
 
 class UserRepository:
+  """
+  Abstracts over our interactions with user data.
+
+  An instance of Postgres pool object is dependency-injected upon
+  instantiation and on each database action, we acquire a free connection
+  from the pool. After we have acquire our wanted results, we then
+  release the connection.
+
+  For added typing benefits, all methods accept a type-hinted class
+  subclassing NamedTuple.
+  """
 
   class UserCreate(NamedTuple):
     first_name: str
@@ -27,92 +38,106 @@ class UserRepository:
     last_login_at: datetime
 
   class UserPrivateView(NamedTuple):
-    class History(NamedTuple):
-      url: str
-      accessed_at: datetime
     first_name: str
     last_name: str
     username: str
     password: str
     joined_at: datetime
     last_login_at: datetime
-    history: List[History]
+    history: List[Tuple[str, datetime]]
 
   def __init__(self, pool: aiopg.Pool):
-    self.pool = pool
+    self. pool = pool
 
   async def create(self, user: UserCreate) -> None:
+    connection = await self.pool.acquire()
+
     current_time = datetime.now(tz=pytz.timezone('US/Eastern'))
-    async with self.pool.cursor() as cursor:
-      await cursor.execute(
-        'INSERT INTO user ('
-        ' firstName, lastName, username, password, joinedAt, lastLoginAt'
-        ') VALUES (%s, %s, %s, %s, %s, %s)', (
-          user.first_name, user.last_name, user.username,
-          user.password, current_time, current_time
-        )
+    await connection.execute(
+      'INSERT INTO user ('
+      ' firstName, lastName, username, password, joinedAt, '
+      ' lastLoginAt ) VALUES (%s, %s, %s, %s, %s, %s)', (
+        user.first_name, user.last_name, user.username,
+        user.password, current_time, current_time
       )
+    )
 
-  async def updateby_id(self, user_id: int, user: UserUpdate) -> UserPublicView:
-    async with self.pool.cursor() as cursor:
-      await cursor.execute(
-        'UPDATE user '
-        'SET firstName=COALESCE(firstName, %s),'
-        ' lastName=COALESCE(lastName, %s), '
-        ' username=COALESCE(username, %s), '
-        ' password=COALESCE(password, %s) '
-        'WHERE id=%s '
-        'RETURNING'
-        ' firstName, lastName, username,'
-        ' password, joinedAt, lastLoginAt', (
-          user.first_name, user.last_name, user.username,
-          user.password, user_id
-        )
+    self.pool.release(connection)
+
+  async def updateby_id(self, user_id: int, user: UserUpdate) -> Union[
+    UserPublicView, None
+  ]:
+    connection = await self.pool.acquire()
+    await connection.execute(
+      'UPDATE user '
+      'SET firstName=COALESCE(firstName, %s),'
+      ' lastName=COALESCE(lastName, %s), '
+      ' username=COALESCE(username, %s), '
+      ' password=COALESCE(password, %s) '
+      'WHERE id=%s '
+      'RETURNING'
+      ' firstName, lastName, username,'
+      ' password, joinedAt, lastLoginAt', (
+        user.first_name, user.last_name, user.username,
+        user.password, user_id
       )
-      updated_user_raw = await cursor.fetchone()
-      return self.UserPublicView(*updated_user_raw)
+    )
+    updated_user_raw = await connection.fetchone()
 
-  async def getby_id_public(self, user_id: int) -> Union[UserPublicView, None]:
-    async with self.pool.cursor() as cursor:
-      await cursor.execute(
-        'SELECT firstName, lastName, username, password, joinedAt, lastLoginAt '
-        'FROM user '
-        'WHERE id=%s', (
-          user_id,
-        )
-      )
-      user_raw = await cursor.fetchone()
+    self.pool.release(connection)
 
-      if user_raw is None:
-        return None
+    if updated_user_raw is None:
+      return None
+    return self.UserPublicView(*updated_user_raw)
 
-      return self.UserPublicView(*user_raw)
+  async def getby_id_public(self, user_id: int) -> Union[
+    UserPublicView, None
+  ]:
+    connection = await self.pool.acquire()
+    await connection.execute(
+      'SELECT firstName, lastName, username, password,'
+      ' joinedAt, lastLoginAt '
+      'FROM user '
+      'WHERE id=%s',
+      (user_id,)
+    )
+    user_raw = await connection.fetchone()
 
-  async def getby_id_private(self, user_id: int) -> Union[UserPrivateView, None]:
+    self.pool.release(connection)
+
+    if user_raw is None:
+      return None
+    return self.UserPublicView(*user_raw)
+
+  async def getby_id_private(self, user_id: int) -> Union[
+    UserPrivateView, None
+  ]:
     public_info_user = await self.getby_id_public(user_id)
 
     if public_info_user is None:
       return None
 
-    async with self.pool.cursor() as cursor:
-      await cursor.execute(
-        'SELECT document.url, history.accessedAt '
-        'FROM history, document '
-        'WHERE history.userId=%s AND history.documentId == document.id', (
-          user_id
-        )
-      )
-      history_list_raw = await cursor.fetchall()
+    connection = await self.pool.acquire()
+    await connection.execute(
+      'SELECT document.url, history.accessedAt '
+      'FROM history, document '
+      'WHERE history.userId=%s AND history.documentId == document.id',
+      (user_id,)
+    )
+    history_list_raw = await connection.fetchall()
+    self.pool.release(connection)
 
-      return self.UserPrivateView(list(public_info_user).append(
-        [self.UserPrivateView.History(*history_raw)
-         for history_raw in history_list_raw]
-      ))
+    params = list(public_info_user)
+    params.append(history_list_raw)
+    return self.UserPrivateView(*params)
 
   async def deleteby_id(self, user_id: int) -> None:
-    async with self.pool.cursor() as cursor:
-      await cursor.execute(
-        'DELETE FROM user WHERE id=%s', (
-          user_id
-        )
+    connection = await self.pool.acquire()
+
+    await connection.execute(
+      'DELETE FROM user WHERE id=%s', (
+        user_id
       )
+    )
+
+    self.pool.release(connection)
---
+++ tests/db/user_test.py
@@ -0,0 +1,136 @@
+from asynctest import TestCase, MagicMock, CoroutineMock
+from aiopg import Pool, Cursor
+from app.db.user import UserRepository
+from datetime import datetime
+
+
+class UserRepositoryTest(TestCase):
+
+  mock_cursor = MagicMock(Cursor)
+  postgres_pool_mock = MagicMock(Pool)
+  postgres_pool_mock.acquire = CoroutineMock(return_value=mock_cursor)
+
+  async def tearDown(self):
+    self.mock_cursor.reset_mock()
+    self.postgres_pool_mock.reset_mock()
+
+  async def test_create(self):
+    user = UserRepository(self.postgres_pool_mock)
+    await user.create(UserRepository.UserCreate(
+      "Joe", "Schmoe", "someUsername", "somePass"
+    ))
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+
+  async def test_delete(self):
+    user = UserRepository(self.postgres_pool_mock)
+    await user.deleteby_id(1)
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+
+  async def test_update_exists(self):
+    joined_at = datetime.now()
+    last_login_at = datetime.now()
+    self.mock_cursor.fetchone = CoroutineMock(return_value=(
+      "firstName", "lastName", "username", "password",
+      joined_at, last_login_at
+    ))
+
+    user = UserRepository(self.postgres_pool_mock)
+    updated_user = await user.updateby_id(1, UserRepository.UserUpdate(
+      None, None, None, None
+    ))
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+    self.mock_cursor.fetchone.assert_called_once()
+
+    self.assertIsInstance(updated_user, UserRepository.UserPublicView)
+    self.assertEqual(updated_user.first_name, "firstName")
+    self.assertEqual(updated_user.last_name, "lastName")
+    self.assertEqual(updated_user.username, "username")
+    self.assertEqual(updated_user.password, "password")
+    self.assertIs(updated_user.joined_at, joined_at)
+    self.assertIs(updated_user.last_login_at, last_login_at)
+
+  async def test_update_notexists(self):
+    self.mock_cursor.fetchone = CoroutineMock(return_value=None)
+
+    user = UserRepository(self.postgres_pool_mock)
+    updated_user = await user.updateby_id(1, UserRepository.UserUpdate(
+      None, None, None, None
+    ))
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+    self.mock_cursor.fetchone.assert_called_once()
+
+    self.assertEqual(updated_user, None)
+
+  async def test_getpublic_exists(self):
+    joined_at = datetime.now()
+    last_login_at = datetime.now()
+    self.mock_cursor.fetchone = CoroutineMock(return_value=(
+      "firstName", "lastName", "username", "password",
+      joined_at, last_login_at
+    ))
+
+    user = UserRepository(self.postgres_pool_mock)
+    obtained_user = await user.getby_id_public(1)
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+    self.mock_cursor.fetchone.assert_called_once()
+
+    self.assertIsInstance(obtained_user, UserRepository.UserPublicView)
+    self.assertEqual(obtained_user.first_name, "firstName")
+    self.assertEqual(obtained_user.last_name, "lastName")
+    self.assertEqual(obtained_user.username, "username")
+    self.assertEqual(obtained_user.password, "password")
+    self.assertIs(obtained_user.joined_at, joined_at)
+    self.assertIs(obtained_user.last_login_at, last_login_at)
+
+  async def test_getpublic_notexists(self):
+    self.mock_cursor.fetchone = CoroutineMock(return_value=None)
+
+    user = UserRepository(self.postgres_pool_mock)
+    obtained_user = await user.getby_id_public(1)
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+    self.mock_cursor.fetchone.assert_called_once()
+
+    self.assertEqual(obtained_user, None)
+
+  async def test_getprivate(self):
+    returned_user = UserRepository.UserPublicView(
+      "firstName", "lastName", "username", "password",
+      datetime.now(), datetime.now()
+
+    )
+
+    history = [("someUrl", datetime.now())]
+    self.mock_cursor.fetchall = CoroutineMock(return_value=history)
+
+    user = UserRepository(self.postgres_pool_mock)
+    user.getby_id_public = CoroutineMock(
+      return_value=returned_user
+    )
+    obtained_user = await user.getby_id_private(1)
+
+    self.postgres_pool_mock.acquire.assert_called_once()
+    self.postgres_pool_mock.release.assert_called_once()
+    self.mock_cursor.execute.assert_called_once()
+    self.mock_cursor.fetchall.assert_called_once()
+    user.getby_id_public.assert_called_once()
+
+    self.assertIsInstance(obtained_user, UserRepository.UserPrivateView)
+    self.assertIs(obtained_user.history, history)
