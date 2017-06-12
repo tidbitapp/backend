161edcdf1c6ea2b38a06622ad747af68e4908991
Alastair Paragas <alastairparagas@gmail.com>
2017-06-12 10:33:55 -0400

    JWT Token Helper

---
+++ app/utils/session_token.py
@@ -0,0 +1,28 @@
+import jwt
+from os import getenv
+from datetime import datetime, timedelta
+import pytz
+from typing import Union
+
+
+def create(payload: dict) -> str:
+  def datetime_future(hours: int):
+    now = datetime.now(tz=pytz.timezone('US/Eastern'))
+    hours_added = timedelta(hours=hours)
+    return now + hours_added
+
+  jwt_secret = getenv('JWT_SECRET')
+  payload['exp'] = datetime_future(24)
+
+  return jwt.encode(payload, jwt_secret, algorithm='HS256')
+
+
+def get_contents(token: str) -> Union[dict, None]:
+  jwt_secret = getenv('JWT_SECRET')
+
+  try:
+    contents = jwt.decode(token, jwt_secret)
+  except jwt.exceptions.InvalidTokenError:
+    return None
+
+  return contents
