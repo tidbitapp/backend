08121c02a95a654f43956012b374f80fbc697518
Alastair Paragas <alastairparagas@gmail.com>
2017-06-24 18:13:45 +0200

    Rename Environment Variables

--- app/db/startup.py
+++ app/db/startup.py
@@ -4,18 +4,18 @@ from os import getenv
 
 async def on_startup(app: Application):
 
-  db_name = getenv("DB_NAME")
-  db_username = getenv("DB_USERNAME")
-  db_password = getenv("DB_PASSWORD")
-  db_host = getenv("DB_HOST")
+  db_name = getenv('POSTGRES_DB_NAME')
+  db_username = getenv('POSTGRES_DB_USERNAME')
+  db_password = getenv('POSTGRES_DB_PASSWORD')
+  db_host = getenv('POSTGRES_DB_HOST')
 
   if not (db_name and db_username and db_password and db_host):
-    raise RuntimeError("DB environment variables not set!")
+    raise RuntimeError('DB environment variables not set!')
 
   dsn = 'dbname={} user={} password={} host={}'.format(
-    getenv('DB_NAME'),
-    getenv('DB_USERNAME'),
-    getenv('DB_PASSWORD'),
-    getenv('DB_HOST')
+    getenv('POSTGRES_DB_NAME'),
+    getenv('POSTGRES_DB_USERNAME'),
+    getenv('POSTGRES_DB_PASSWORD'),
+    getenv('POSTGRES_DB_HOST')
   )
   app['db_pool'] = aiopg.create_pool(dsn=dsn)
