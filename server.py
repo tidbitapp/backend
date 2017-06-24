060b231a6c3a866d8d450d12d47b83d1b3f285b5
Alastair Paragas <alastairparagas@gmail.com>
2017-06-24 18:14:07 +0200

    Bugfix: path instead of patch

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import aiohttp.web
 import app.UserController as User
-from .app.AuthenticateController import authenticate
-from .app.SummaryController import summarize
-from .app.db.startup import on_startup as db_on_startup
+from app.AuthenticateController import authenticate
+from app.SummaryController import summarize
+from app.db.startup import on_startup as db_on_startup
 
 async def index() -> aiohttp.web.Response:
   return aiohttp.web.Response(
@@ -14,7 +14,7 @@ app = aiohttp.web.Application()
 
 app.router.add_route(method="*", path="/", handler=index)
 app.router.add_post(path="/user", handler=User.create)
-app.router.add_get(patch="/user/{user_id}", handler=User.get)
+app.router.add_get(path="/user/{user_id}", handler=User.get)
 app.router.add_post(path="/user/{user_id}", handler=User.update)
 app.router.add_delete(path="/user/{user_id}", handler=User.delete)
 app.router.add_post(path="/authenticate", handler=authenticate)
