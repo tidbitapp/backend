eae3246fd584706728d21c5caf4d86626904cb59
Alberto Mizrahi <amb110395@gmail.com>
2017-07-06 15:29:30 -0400

    Implemented the history API routes

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from os import getenv
 import app.UserController as User
 from app.AuthenticateController import authenticate
 import app.SummaryController as Summary
+import app.HistoryController as History
 from app.db.startup import on_startup as db_on_startup
 from app.db.cleanup import on_cleanup as db_on_cleanup
 
@@ -22,6 +23,7 @@ app.router.add_delete(path="/user/{user_id}", handler=User.delete)
 app.router.add_post(path="/authenticate", handler=authenticate)
 app.router.add_post(path="/summary", handler=Summary.summarize)
 app.router.add_get(path="/summary/types", handler=Summary.get_summarizer_types)
+app.router.add_get(path="/user/{user_id}/history", handler=History.index)
 
 app.on_startup.append(db_on_startup)
 app.on_cleanup.append(db_on_cleanup)
